dc513ad89b309f5f8eef8191bd16678cf02d44fe
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2019-08-09 14:08:43 +0200

    Implement ValidationComponent

--- src/ValidationComponent/ValidationComponent.js
+++ src/ValidationComponent/ValidationComponent.js
@@ -1,13 +1,15 @@
 import React from "react";
 
 const ValidationComponent = props => {
+	let validationMessage = "Text long enough.";
+
+	if (props.inputLength < 5) {
+		validationMessage = "Text too short!";
+	}
+
 	return (
 		<div>
-			{props.inputLength >= 5 ? (
-				<p>Text long enough.</p>
-			) : (
-				<p>Text is too short!</p>
-			)}
+			<p>{validationMessage}</p>
 		</div>
 	);
 };
